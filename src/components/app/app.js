b9eb57ec90b6687f535644e4da3a6b8b0d0a08cc
Boriss Arhipov <bori.7166455@gmail.com>
2020-11-02 14:25:40 +0200

    Implementing a quick sort

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,33 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("bootstrap-css-only");
 require("./app.css");
 class Visualizer {
     constructor() {
         this.sizeInput = document.querySelector('#size');
+        this.randomizeBtn = document.querySelector('#randomize');
+        this.sortType = document.querySelector('#select');
+        this.sortBtn = document.querySelector('#sort');
         this.render = document.querySelector('.visualizer--render');
         this.array = [];
         this.fillArray(Number(this.sizeInput.value));
-        this.draw();
+        this.draw(this.array);
+        this.arrayResize();
+        this.arrayRandomize();
+        this.sort();
     }
     ;
     fillArray(arraySize) {
+        this.array = [];
         while (arraySize) {
             let div = document.createElement('div');
             let randomNum = Math.floor(Math.random() * 100);
@@ -25,14 +41,84 @@ class Visualizer {
             arraySize--;
         }
     }
-    draw() {
+    draw(array) {
         this.render.innerHTML = '';
-        for (let i = 0; i < this.array.length; i++) {
+        for (let i = 0; i < array.length; i++) {
             let progress = document.createElement('div');
             progress.classList.add('progress');
-            progress.appendChild(this.array[i]);
+            progress.appendChild(array[i]);
             this.render.appendChild(progress);
         }
+    }
+    sort() {
+        this.sortBtn.addEventListener('click', () => {
+            switch (this.sortType.value) {
+                case "quick-sort":
+                    this.quickSort(this.array, 0, this.array.length - 1);
+                default:
+                    return;
+            }
+        });
+    }
+    arrayResize() {
+        this.sizeInput.addEventListener('change', () => {
+            this.fillArray(Number(this.sizeInput.value));
+            this.draw(this.array);
+        });
+    }
+    arrayRandomize() {
+        this.randomizeBtn.addEventListener('click', () => {
+            let j, x, i;
+            for (i = this.array.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = this.array[i];
+                this.array[i] = this.array[j];
+                this.array[j] = x;
+            }
+            this.draw(this.array);
+        });
+    }
+    quickSort(array, start, end) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (start >= end) {
+                this.draw(array);
+                return;
+            }
+            let index = yield this.partition(array, start, end);
+            console.log(array.map(item => item.getAttribute('aria-valuenow')));
+            yield Promise.all([
+                this.quickSort(array, start, index - 1),
+                this.quickSort(array, index + 1, end)
+            ]);
+        });
+    }
+    partition(array, start, end) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let pivotIndex = start;
+            let pivotValue = Number(array[end].getAttribute('aria-valuenow'));
+            for (let i = start; i < end; i++) {
+                if (Number(array[i].getAttribute('aria-valuenow')) < pivotValue) {
+                    yield this.swap(array, i, pivotIndex);
+                    pivotIndex++;
+                }
+                this.draw(array);
+            }
+            yield this.swap(array, pivotIndex, end);
+            return pivotIndex;
+        });
+    }
+    swap(arr, a, b) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.sleep(100);
+            let temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        });
+    }
+    sleep(ms) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        });
     }
 }
 new Visualizer();
--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -3,27 +3,30 @@ import './app.css';
 
 class Visualizer {
     private sizeInput: HTMLInputElement;
-    // private randomizeBtn: HTMLButtonElement;
-    // private sortType: HTMLSelectElement;
-    // private sortBtn: HTMLButtonElement;
+    private randomizeBtn: HTMLButtonElement;
+    private sortType: HTMLSelectElement;
+    private sortBtn: HTMLButtonElement;
     private array: HTMLDivElement[];
     private render: HTMLDivElement;
 
     constructor() {
         this.sizeInput = document.querySelector('#size') as HTMLInputElement;
-        // this.randomizeBtn = document.querySelector('#randomize') as HTMLButtonElement;
-        // this.sortType = document.querySelector('#select') as HTMLSelectElement;
-        // this.sortBtn = document.querySelector('#sort') as HTMLButtonElement;
+        this.randomizeBtn = document.querySelector('#randomize') as HTMLButtonElement;
+        this.sortType = document.querySelector('#select') as HTMLSelectElement;
+        this.sortBtn = document.querySelector('#sort') as HTMLButtonElement;
         this.render = document.querySelector('.visualizer--render') as HTMLDivElement;
 
         this.array = [];
 
         this.fillArray(Number(this.sizeInput.value));
-
-        this.draw();
+        this.draw(this.array);
+        this.arrayResize();
+        this.arrayRandomize();
+        this.sort();
     };
 
     fillArray(arraySize: number) {
+        this.array = [];
         while(arraySize) {
             let div: HTMLDivElement  = document.createElement('div');
             let randomNum: number = Math.floor(Math.random() * 100);
@@ -38,15 +41,90 @@ class Visualizer {
         }
     }
 
-    draw() {
+    draw(array: HTMLDivElement[]) {
         this.render.innerHTML = '';
-        for(let i = 0; i < this.array.length; i++) {
+        for(let i = 0; i < array.length; i++) {
             let progress: HTMLDivElement = document.createElement('div');
             progress.classList.add('progress');
-            progress.appendChild(this.array[i]);
+            progress.appendChild(array[i]);
             this.render.appendChild(progress);
         }
+    }
+
+    sort() {
+        this.sortBtn.addEventListener('click', () => {
+            switch(this.sortType.value) {
+                case "quick-sort":
+                    this.quickSort(this.array, 0, this.array.length - 1);
+                default: 
+                    return;
+            }
+        });
+    }
+
+    arrayResize() {
+        this.sizeInput.addEventListener('change', () => {
+            this.fillArray(Number(this.sizeInput.value));
+            this.draw(this.array);
+        });
+    }
+
+    arrayRandomize() {
+        this.randomizeBtn.addEventListener('click', () => {
+            let j: number,
+                x: HTMLDivElement, 
+                i: number;
+            for (i = this.array.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = this.array[i];
+                this.array[i] = this.array[j];
+                this.array[j] = x;
+            }
+            this.draw(this.array);
+        });
+    }
+
+    async quickSort(array: HTMLDivElement[], start: number, end: number):Promise<any> {
+        if (start >= end) {
+            this.draw(array);
+            return;
+        }
+
+        let index = await this.partition(array, start, end);
+        console.log(array.map(item => item.getAttribute('aria-valuenow')));
+        await Promise.all([
+            this.quickSort(array, start, index - 1),
+            this.quickSort(array, index + 1, end)
+        ]);
+        
+    }
+
+    async partition(array: HTMLDivElement[], start: number, end: number):Promise<any> {
+        let pivotIndex: number = start;
+        let pivotValue: number = Number(array[end].getAttribute('aria-valuenow'));
+
+        for(let i = start; i < end; i++) {
+            if(Number(array[i].getAttribute('aria-valuenow')) < pivotValue) {
+                await this.swap(array, i, pivotIndex);
+                pivotIndex++;
+            }
+            this.draw(array);
+        }
+        await this.swap(array, pivotIndex, end);
+        return pivotIndex;
+    }
+
+    async swap(arr: HTMLDivElement[], a: number, b: number) {
+        await this.sleep(100);
+        let temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;  
+    }
+
+    async sleep(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
 
 new Visualizer();
+
